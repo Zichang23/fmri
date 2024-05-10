3eeee468853225529da12b459401939953fb09a2
Zichang23 <84609614+Zichang23@users.noreply.github.com>
2024-05-10 11:01:47 -0500

    Update hboot2.R

--- functions/hboot2.R
+++ functions/hboot2.R
@@ -1,29 +1,29 @@
 library(iZID)
 
 #function to check dist for x1 or x2
-check.dist <- function(x1,x2){#x1,x2 are vectors
+check.dist <- function(x1, x2, alpha=0.05, B=100){#x1,x2 are vectors
   # d1=dis.kstest(x1,nsim=100,bootstrap=TRUE,distri='zip')
   # d2=dis.kstest(x1,nsim=100,bootstrap=TRUE,distri='zinb')
-  d3=dis.kstest(x1,nsim=100,bootstrap=TRUE,distri='Poisson')
-  d4=dis.kstest(x1,nsim=100,bootstrap=TRUE,distri='nb')
+  d3=dis.kstest(x1,nsim=B,bootstrap=TRUE,distri='Poisson')
+  d4=dis.kstest(x1,nsim=B,bootstrap=TRUE,distri='nb')
   pval1 <- c(d3$pvalue, d4$pvalue)
   llik1 <- round(c(d3$mle_ori[2], d4$mle_ori[3]),5)
   dist1 <- c("Poisson", "nb")
   id <- 1:2
   dat1 <- data.frame(cbind(id, dist1, pval1, llik1))
-  dat2 <- dat1[dat1$pval1 >=0.05,]
+  dat2 <- dat1[dat1$pval1 >=alpha,]
   dat3 <- dat2[dat2$llik1==max(as.numeric(dat2$llik1)),]
   dat4 <- dat3[dat3$pval1==max(as.numeric(dat3$pval1)),]
   avg1 <- round(mean(x1),4)
   var1 <- round(var(x1),4)
   result1 <- cbind(dat4[,1:2], avg1, var1)
-  d7=dis.kstest(x2,nsim=100,bootstrap=TRUE,distri='Poisson')
-  d8=dis.kstest(x2,nsim=100,bootstrap=TRUE,distri='nb')
+  d7=dis.kstest(x2,nsim=B,bootstrap=TRUE,distri='Poisson')
+  d8=dis.kstest(x2,nsim=B,bootstrap=TRUE,distri='nb')
   pval2 <- c(d7$pvalue, d8$pvalue)
   llik2 <- round(c(d7$mle_ori[2], d8$mle_ori[3]),5)
   dist2 <- c("Poisson", "nb")
   dat5 <- data.frame(cbind(id, dist2, pval2, llik2))
-  dat6 <- dat5[dat5$pval2 >=0.05,]
+  dat6 <- dat5[dat5$pval2 >=alpha,]
   dat7 <- dat6[dat6$llik2==max(as.numeric(dat6$llik2)),]
   dat8 <- dat7[dat7$pval2 == max(as.numeric(dat7$pval2)),]
   avg2 <- round(mean(x2),4)
@@ -37,7 +37,7 @@ check.dist <- function(x1,x2){#x1,x2 are vectors
 }
 
 #function to compare ASD gorup (x1) and control group (x2)
-comp.dist1 <- function(x1, x2, B=500){#x1,x2 are vectors
+comp.dist1 <- function(x1, x2, alpha=0.05, B=500){#x1,x2 are vectors
   aa <- check.dist1(x1,x2)
   dist1 <- as.numeric(aa[1,1])
   dist2 <- as.numeric(aa[2,1])
@@ -66,7 +66,7 @@ comp.dist1 <- function(x1, x2, B=500){#x1,x2 are vectors
       }
       # boot.stat <- abs(boot.avg1-boot.avg2)
       # pval <- mean(boot.stat >= abs(mean(x1)-mean(x2)))
-      hyp <- ifelse(pval>0.05, "H0", "H1")
+      hyp <- ifelse(pval>alpha, "H0", "H1")
       avg1 <- round(mean(x1),4)
       avg2 <- round(mean(x2),4)
       result <- data.frame(hyp, avg1, avg2)
@@ -93,7 +93,7 @@ comp.dist1 <- function(x1, x2, B=500){#x1,x2 are vectors
       }
       # boot.stat <- abs(boot.avg1-boot.avg2)
       # pval <- mean(boot.stat >= abs(mean(x1)-mean(x2)))
-      hyp <- ifelse(pval>0.05, "H0", "H1")
+      hyp <- ifelse(pval>alpha, "H0", "H1")
       avg1 <- round(mean(x1),4)
       avg2 <- round(mean(x2),4)
       result <- data.frame(hyp, avg1, avg2)
